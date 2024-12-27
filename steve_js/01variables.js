6138bcc8896c9a5369eb4526d1d068644b717d8c
Steve-Yt <ksujeet7091@gmail.com>
2024-12-27 18:50:13 +0530

    Create 01variables.js

---
+++ steve_js/01variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453;
+let accountEmail = "ksujeet7091@gmail.com";
+var accountPassword = "12345"
+accountCity = "jaipur";
+
+// accountId = 2  //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
+console.log(account);
